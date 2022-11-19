06877eb98f663dd4d91d2db6b283aca5f665e8fd
conggao <578422314@qq.com>
2022-11-19 12:44:42 +0800

    数据库初始化脚本

---
+++ database/schema.sql
@@ -0,0 +1,34 @@
+create database `visual-proj` character set utf8mb4 collate utf8mb4_unicode_ci;
+create table vp_datasource
+(
+    id          int auto_increment
+        primary key,
+    name        varchar(255)                         not null,
+    comment     text                                 null,
+    create_time timestamp  default CURRENT_TIMESTAMP not null,
+    create_user varchar(50)                          not null,
+    update_time timestamp  default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP,
+    update_user varchar(50)                          not null,
+    enable      tinyint(1) default 1                 not null
+);
+
+create table vp_user
+(
+    id          bigint auto_increment comment '主键'
+        primary key,
+    user_name   varchar(64) default 'NULL' not null comment '用户名',
+    nick_name   varchar(64) default 'NULL' not null comment '昵称',
+    password    varchar(64) default 'NULL' not null comment '密码',
+    enable      char        default '0'    null comment '账号状态（0停用 1正常）',
+    email       varchar(64)                null comment '邮箱',
+    phone       varchar(32)                null comment '手机号',
+    sex         char                       null comment '用户性别（0男，1女，2未知）',
+    avatar      varchar(128)               null comment '头像',
+    user_type   char        default '1'    not null comment '用户类型（0管理员，1普通用户）',
+    create_user varchar(50)                null comment '创建人的用户id',
+    create_time datetime                   null comment '创建时间',
+    update_user varchar(50)                null comment '更新人',
+    update_time datetime                   null comment '更新时间',
+    del_flag    int         default 0      null comment '删除标志（0代表未删除，1代表已删除）'
+)
+    comment '用户表';
---
+++ database/vp_user.sql
@@ -0,0 +1,4 @@
+INSERT INTO `visual-proj`.vp_user (id, user_name, nick_name, password, enable, email, phone, sex, avatar, user_type,
+                                   create_user, create_time, update_user, update_time, del_flag)
+VALUES (1, 'gc', 'NULL', '$2a$10$iCMQ5faUkXT9hqM4YB9pWeKAb7dk02hiJIQvMbeljZQLPN8fqDahq', '1', null, null, null, null,
+        '1', 'system', null, 'system', null, 0);
