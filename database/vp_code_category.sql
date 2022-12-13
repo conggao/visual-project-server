183ebb296d1109ff4bb3642d84f8f34189429cd7
conggao <578422314@qq.com>
2022-12-13 08:03:30 +0800

    导出本地数据库

---
+++ database/vp_code_category.sql
@@ -0,0 +1,4 @@
+INSERT INTO `visual-proj`.vp_code_category (id, parent_id, name, create_user, create_time, update_user, update_time, del_flag) VALUES (1, 0, '前端', 'gc', '2022-12-07 21:04:15', null, '2022-12-07 21:04:15', 0);
+INSERT INTO `visual-proj`.vp_code_category (id, parent_id, name, create_user, create_time, update_user, update_time, del_flag) VALUES (2, 0, '后端', 'gc', '2022-12-07 21:35:46', null, '2022-12-07 21:35:46', 0);
+INSERT INTO `visual-proj`.vp_code_category (id, parent_id, name, create_user, create_time, update_user, update_time, del_flag) VALUES (3, 0, '运维', 'gc', '2022-12-07 21:36:30', null, '2022-12-07 21:36:30', 0);
+INSERT INTO `visual-proj`.vp_code_category (id, parent_id, name, create_user, create_time, update_user, update_time, del_flag) VALUES (4, 1, 'UI组件', 'gc', '2022-12-08 07:10:14', null, '2022-12-08 07:10:14', 0);
---
+++ database/vp_code_segment.sql
@@ -0,0 +1,17 @@
+INSERT INTO `visual-proj`.vp_code_segment (id, type, category_id, title, language, content, create_user, create_time, update_user, update_time, del_flag) VALUES (15, 2, 0, 'vue_base', 'JavaScript', 'export default {
+  name: ''<%= component_name %>'',
+  props: {
+
+  },
+  data () {
+    return {
+    }
+  },
+  computed: {
+
+  },
+  methods: {
+
+  },
+}
+', 'gc', '2022-12-11 22:39:12', 'gc', '2022-12-11 23:08:21', 0);
